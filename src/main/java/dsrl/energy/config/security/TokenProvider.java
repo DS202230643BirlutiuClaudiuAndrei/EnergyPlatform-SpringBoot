8a40465ad0a6c98ed7a94960fa51979cd9878993
Birlutiu Claudiu <birlutiuclaudiuc@gmail.com>
2022-10-22 11:48:38 +0300

    added valid method to check if token from request is valid

--- src/main/java/dsrl/energy/config/security/TokenProvider.java
+++ src/main/java/dsrl/energy/config/security/TokenProvider.java
@@ -2,15 +2,18 @@ package dsrl.energy.config.security;
 
 import dsrl.energy.config.general.TokenProperties;
 import dsrl.energy.model.entity.EnergyUser;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @RequiredArgsConstructor
 public class TokenProvider implements Serializable {
@@ -22,6 +25,7 @@ public class TokenProvider implements Serializable {
 
     /**
      * This method construct the token at authentication
+     *
      * @param energyUser the energy user who is authenticated
      * @return a string which represents the token generated
      */
@@ -41,6 +45,22 @@ public class TokenProvider implements Serializable {
                 .compact();
     }
 
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsTFunction) {
+        Claims claims = Jwts.parser().setSigningKey(tokenProperties.getTokenSecret()).parseClaimsJws(token).getBody();
+        return claimsTFunction.apply(claims);
+    }
 
+    /**
+     * This method check if the token is valid
+     *
+     * @param token      the token extract rom http request header
+     * @param energyUser the user from db
+     * @return a boolean response based on condition of username and expiration date
+     */
+    public boolean isValidToken(String token, UserDetails energyUser) {
+        String usernameFromToken = getClaimFromToken(token, Claims::getSubject);
+        Date expirationDateFromToken = getClaimFromToken(token, Claims::getExpiration);
+        return (expirationDateFromToken.before(new Date(System.currentTimeMillis()))) && usernameFromToken.equals(energyUser.getUsername());
+    }
 
 }
